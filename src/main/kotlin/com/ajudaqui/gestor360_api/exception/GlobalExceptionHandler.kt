b84dea301e6466f13774e8387f8caa8a60716cde
gustavo.paes <gustavo910@gmail.com>
2024-12-20 00:36:05 -0300

    refactor: improvment log error

--- src/main/kotlin/com/ajudaqui/gestor360_api/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/ajudaqui/gestor360_api/exception/GlobalExceptionHandler.kt
@@ -1,10 +1,8 @@
 package com.ajudaqui.gestor360_api.exception
 
-import jakarta.persistence.EntityNotFoundException
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.validation.FieldError
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -14,34 +12,44 @@ import org.springframework.web.context.request.WebRequest
 class GlobalExceptionHandler {
     private val logger = LoggerFactory.getLogger(GlobalExceptionHandler::class.java)
 
-//
+
     @ExceptionHandler(Exception::class)
     fun handleException(
         ex: Exception,
         request: WebRequest
     ): ResponseEntity<ErrorResponse> {
         val status = exceptionStatus[ex::class.java] ?: HttpStatus.INTERNAL_SERVER_ERROR
-        logException(ex)
+        val messageException = formatMessageException(ex)
+
+        logger.error(messageException)
 
         val errorResponse = ErrorResponse(
-            message = ex.message ?: "Erro desconhecido",
+            message = messageException,
             details = request.getDescription(false)
         )
 
         return ResponseEntity(errorResponse, status)
     }
-    private fun logException(ex: Exception) {
-        val stackTraceElement = ex.stackTrace.firstOrNull()
 
-        val simpleClassName = stackTraceElement?.className?.substringAfterLast(".") ?: "UnknownClass"
-        val methodName = stackTraceElement?.methodName ?: "UnknownMethod"
-        val lineNumber = stackTraceElement?.lineNumber ?: -1
+    private fun formatMessageException(ex: Exception): String {
+        val stackTraceElement = ex.stackTrace
+            .firstOrNull { it.className.startsWith("com.ajudaqui.gestor360_api") }
 
-        val callerInfo = "Exception occurred in $simpleClassName.$methodName() at line $lineNumber"
-        val errorMessage = "Error: ${ex.message}. Called by: $callerInfo"
-        logger.error(errorMessage)
+        val callerInfo: String = if (stackTraceElement != null) {
+
+            val simpleClassName = stackTraceElement.className.substringAfterLast(".")
+            val methodName = stackTraceElement.methodName
+            val lineNumber = stackTraceElement.lineNumber
+
+            "Error of type ${ex::class.java.simpleName}: ${ex.message} in $simpleClassName.$methodName() at line $lineNumber"
+        } else {
+            "Error: ${ex.message}. No relevant stack trace found."
+        }
+        return callerInfo
     }
-    private  val exceptionStatus = mapOf(
+
+
+    private val exceptionStatus = mapOf(
         NoSuchElementException::class.java to HttpStatus.NOT_FOUND,
         MethodArgumentNotValidException::class.java to HttpStatus.BAD_REQUEST,
         IllegalArgumentException::class.java to HttpStatus.BAD_REQUEST,
@@ -50,16 +58,6 @@ class GlobalExceptionHandler {
         NoSuchElementException::class.java to HttpStatus.NOT_FOUND,
         NotAutorizationException::class.java to HttpStatus.UNAUTHORIZED
     )
-
-
-
-
-
-
-
-
-
-
 
 
 }
